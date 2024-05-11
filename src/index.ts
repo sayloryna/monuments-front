221c1683b19217bf8f3b7993c60010b965f7ad7a
Clara <clarafdezfraga@gmail.com>
2024-05-11 20:13:42 +0200

    Modularize header and mainCointainer and they styles files

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import createMainHeader from "./header.ts";
+import renderMainContainer from "./mainContainer/mainContainer";
 import "./style.css";
-
-const renderMainContainer = () => {
-  const root = document.querySelector(".root");
-
-  const mainContainer = document.createElement("div");
-  mainContainer.classList.add("main-container");
-
-  const header = createMainHeader("Monumentos del Mundo");
-  if (!root) {
-    throw Error("Element not found");
-  }
-
-  mainContainer.appendChild(header);
-  root.appendChild(mainContainer);
-};
-
 renderMainContainer();
